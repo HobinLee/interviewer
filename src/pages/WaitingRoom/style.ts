57aaeab85756e4ae3b9c4801c53804ac15bf91c3
HobinLee <viny5120@gmail.com>
2021-09-11 23:25:16 +0900

    feat: 대기실 스타일링

---
+++ src/pages/WaitingRoom/style.ts
@@ -0,0 +1,46 @@
+import style from "styled-components";
+
+export const WaitingRoomWrapper = style.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 90vh;
+  width: 100%;
+
+  .waiting-room {
+    display: flex;
+    > div {
+      border-radius: 1rem;
+    }
+    &__camera {
+      min-width: 400px;
+      width: 50vw;
+      height: 28vw;
+      background: #333;
+    }
+
+    &__start {
+      width: 44.8rem;
+      min-height: 100%;
+      display: flex;
+      flex-direction: column;
+      text-align: center;
+      justify-content: center;
+      align-items: center;
+
+      > h3 {
+        font-size: 2.8rem;
+      }
+
+      > a {
+        font-size: 1.6rem;
+        display: block;
+        padding: 1.7rem 3rem;
+        background: #1a73e8;
+        color: #fff;
+        border-radius: 2.5rem;
+        text-decoration: none;
+      }
+    }
+  }
+`
