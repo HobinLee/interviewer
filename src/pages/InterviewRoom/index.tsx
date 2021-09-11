6129c42bc2198eee0c2a51331a125362d14041b6
HobinLee <viny5120@gmail.com>
2021-09-12 00:23:16 +0900

    feat: 인터뷰실 스타일링

--- src/pages/InterviewRoom/index.tsx
+++ src/pages/InterviewRoom/index.tsx
@@ -1,6 +1,10 @@
-import React, {useState, useEffect} from 'react';
-import { Link } from 'react-router-dom';
-import { questions } from '../../questions';
+import React, {useState, useEffect} from "react";
+import { Link } from "react-router-dom";
+import { questions } from "../../questions";
+import { InterviewRoomWrapper } from "./style";
+import { MdCallEnd } from "react-icons/md";
+import { GrNext } from "react-icons/gr";
+import { AiOutlineFileSearch } from "react-icons/ai";
 
 const MILLSEC_PER_SEC :number = 1000;
 
@@ -19,13 +23,34 @@ const InterviewRoom = () => {
     setTimer(now);
   }
 
-  return <div>
-    <h2>
-      {restQuestions[0] ?? '고생하셨습니다!'}
-    </h2>
-    {restQuestions[0] ? <button onClick={handelNextQuestion}>next</button>:
-    <Link to="/review">review</Link>}
-  </div>
+  return <InterviewRoomWrapper>
+    <div className="interviewers">
+      <div className="interviewer">
+        <div className="profile"></div>
+        <h3 className="question">"{restQuestions[0] ?? "고생하셨습니다"}"</h3>
+      </div>
+      <div className="interviewer">
+        <div className="profile"></div>
+      </div>
+    </div>
+    <div className="interviewee">
+      <div className="profile"></div>
+    </div>
+    <div className="footer">
+      {restQuestions[0] ?
+        <button className="next-button" onClick={handelNextQuestion}>
+          <GrNext/>
+        </button>
+          :
+        <Link className="review-button" to="/review">
+          <AiOutlineFileSearch/>
+        </Link>
+      }
+      <Link className="close-button" to="/">
+        <MdCallEnd/>
+      </Link>
+    </div>
+  </InterviewRoomWrapper>
 }
 
 export default InterviewRoom;
---
+++ src/pages/InterviewRoom/style.ts
@@ -0,0 +1,102 @@
+import style from "styled-components";
+
+export const InterviewRoomWrapper = style.div`
+  background: #333;
+  color: #fff;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  position: relative;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  h3 {
+    font-size: 2.8rem;
+    margin: 2rem 0 0 0;
+  }
+
+  .interviewers {
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    height: calc(25vw + 13em);
+    max-height: ;
+
+    > div {
+      width: 40vw;
+      max-width: 600px;
+      margin: 2rem;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      text-align: center
+    }
+  }
+
+  .profile {
+    width: 40vw;
+    height: 25vw;
+    max-width: 600px;
+    max-height: 375px;
+    background: #222;
+    border-radius: 2rem;
+  }
+ 
+  .footer {
+    justify-self: flex-end;
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    padding: 1rem 0;
+    display: flex;
+    justify-content: center;
+
+    .next-button {
+      margin: 2rem;
+      svg {
+        width: 2rem;
+        height: 2rem;
+        > polyline { 
+          stroke: #fff;
+        }
+      }
+
+      padding: 0.8rem 2.8rem;
+      background: #1a73e8;
+      color: #fff;
+      border-radius: 2.5rem;
+      text-decoration: none;
+      outline: none;
+      border: none;
+      cursor: pointer;
+    }
+
+    a {
+      margin: 2rem;
+      svg {
+        width: 3rem;
+        height: 3rem;
+      }
+      padding: 0.8rem 2.8rem;
+      color: white;
+      border-radius: 2.3rem;
+      text-decoration: none;
+    }
+
+    .close-button {
+
+      background: #ea4335;
+    }
+
+    .review-button {
+      background: #1a73e8;
+    }
+  }
+
+  .interviewee {
+    position: relative;
+  }
+`
