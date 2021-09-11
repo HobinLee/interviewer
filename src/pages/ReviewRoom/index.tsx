df2a7d391d456ae6cd0960332f983542db4bad2b
HobinLee <viny5120@gmail.com>
2021-09-12 00:28:04 +0900

    feat: review room 세팅

--- src/pages/ReviewRoom/index.tsx
+++ src/pages/ReviewRoom/index.tsx
@@ -1,6 +1,7 @@
-import React, {useState, useEffect} from 'react';
-import { Link } from 'react-router-dom';
+import React, {useState, useEffect} from "react";
+import { Link } from "react-router-dom";
+import { ReviewRoomWrapper } from "./style";
 
-const ReviewRoom = () => <div> Review <Link to="/">again</Link></div>
+const ReviewRoom = () => <ReviewRoomWrapper> Review <Link to="/">again</Link></ReviewRoomWrapper>
 
 export default ReviewRoom;
